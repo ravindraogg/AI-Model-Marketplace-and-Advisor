0f38bf929515288174b0c192020d4e14639badaa
PanatiNitesh <niteshreddy242005@gmail.com>
2025-10-04 22:28:03 +0530

    solved backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
